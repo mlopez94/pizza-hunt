db6f66ad010dd2f1a170bfa5c8229f6ba2adad6a
mlopez <lopezmatthew87@gmail.com>
2021-08-19 21:22:45 -0400

    getting ready for Heroku

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
 // create the Pizza model using the PizzaSchema
 
@@ -8,18 +8,25 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: "Large",
+      
     },
     toppings: [],
     comments: [
@@ -32,15 +39,18 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
     id: false,
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 const Pizza = model("Pizza", PizzaSchema);
